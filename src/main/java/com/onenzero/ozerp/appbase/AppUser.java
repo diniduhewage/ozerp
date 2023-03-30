76f2bf5f9efa7c20d7851100cef5b996f58ecf1d
diniduhewage <mailtodinidu@gmail.com>
2023-03-30 08:57:34 +0530

    user commit and configuration correction

--- src/main/java/com/onenzero/ozerp/appbase/AppUser.java
+++ src/main/java/com/onenzero/ozerp/appbase/AppUser.java
@@ -1,5 +1,58 @@
 package com.onenzero.ozerp.appbase;
 
-public class AppUser {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user_tab")
+public class AppUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Boolean activeUser;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
